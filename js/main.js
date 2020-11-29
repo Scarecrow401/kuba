4c594b51cd4cda820b8f9f740b6071fb8e6e42ec
ScareCrow401 <scarecrow4018@gmail.com>
2020-11-29 15:53:01 +0500

    implemented responsive

--- js/main.js
+++ js/main.js
@@ -2,11 +2,9 @@ $(function () {
     $('.photo__slider').slick({
         prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
-        // responsive: [{
-        //     breakpoint: 601,
-        //     settings: {
-        //         arrows: false
-        //     }
-        // }]
-    })
+    });
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').slideToggle();
+    });
 });
